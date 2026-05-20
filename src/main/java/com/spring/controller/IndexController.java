62f19fab86319902c33038466a67c8a624c446b6
saiaungzawoo <saimon39999@gmail.com>
2026-05-20 11:46:07 +0630

    Update IndexController.java

--- src/main/java/com/spring/controller/IndexController.java
+++ src/main/java/com/spring/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //github test
+//github test 2
 
 @Controller
 public class IndexController {
